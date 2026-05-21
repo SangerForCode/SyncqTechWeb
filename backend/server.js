700b2e4c0622d90a392f275a2e66af8bdd02bc9d
Ayush Sanger <sangerforbusiness@gmail.com>
2026-05-21 18:21:54 +0530

    s3 zindabad

--- backend/server.js
+++ backend/server.js
@@ -22,8 +22,8 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/api", applyRoutes);
 app.use("/api", adminRoutes);
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
